99bb6bc251869aebabf47dd6f5bd4a7110c2b0ca
PieroLS04 <piero@datacurso.com>
2026-07-22 16:57:40 -0500

    feat: enhance access control for get_progress and get_token functions; add tests for both

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_assign_ai';
 $plugin->release = '1.1.7';
-$plugin->version = 2026110317;
+$plugin->version = 2026110318;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 501];
