c140ab7610c72e95d0e2cd257f6904217b545a8d
PieroLS04 <piero@datacurso.com>
2026-07-24 11:23:23 -0500

    fix: release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_assign_ai';
-$plugin->release = '1.1.6';
+$plugin->release = '1.1.5';
 $plugin->version = 2026110318;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
